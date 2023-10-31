bb625ade23347c0be61740ac66c7974878801b60
hunderaweke <hunderaweke@gmail.com>
2023-10-31 11:40:11 +0300

    updated: readme

--- file_tracker.py
+++ file_tracker.py
@@ -7,4 +7,5 @@ untracked_files = repo.untracked_files
 for untracked_file in untracked_files:
     repo.git.add(f=untracked_file)
     repo.git.commit(m=f"added: {untracked_file}")
-repo.git.push()
+origin = repo.remote(name='origin')
+origin.push()
