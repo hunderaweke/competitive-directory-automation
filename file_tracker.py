f610909ec4fafc1722f4534426c607cd989ee501
hunderaweke <hunderaweke@gmail.com>
2023-10-31 13:29:47 +0300

    updated: readme writing method

--- file_tracker.py
+++ file_tracker.py
@@ -14,9 +14,15 @@ if len(untracked_files[0]):
     for untracked_file in untracked_files:
         repo.git.add(untracked_file[0])
         logging.info(f"added: {untracked_file[0]}")
-        sentence = f"{untracked_file[0].split('.')[0]}"
-        readme.write(f"{sentence}")
-        repo.git.commit(m=f"added: {untracked_file[0]}")
+        problem_name = input("Enter the name of the problem: ")
+        problem_number = input("Enter the number of the problem: ")
+        problem_difficulty = input("Enter difficulty level: ")
+        problem_link = input("Enter problem link: ")
+        platform = input("Enter plaform")
+        readme.write(
+            f"| {problem_number} | [{problem_name}]({problem_link}) | {problem_difficulty} | | {platform}| ||[{'Python' if 'py' in untracked_file[0] else 'C++' }](./{untracked_file[0]})|"
+        )
+        repo.git.commit(m=f"solved: {problem_name}")
 logging.info("Initializing the remote origin for push")
 origin = repo.remote(name="origin")
 logging.info(f"origin: {origin.url}")
