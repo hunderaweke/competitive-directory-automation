ea5b303c0e663945e96da3045eb66d9d0261413c
hunderaweke <hunderaweke@gmail.com>
2023-10-31 11:53:57 +0300

    added: file_tracker.py

--- file_tracker.py
+++ file_tracker.py
@@ -6,8 +6,7 @@ path = os.getenv("DIR")
 repo = Repo(path)
 untracked_files = [i.split()[1:] for i in repo.git.status("--porcelain").split("\n")]
 for untracked_file in untracked_files:
-    repo.git.add(f=untracked_file[0])
-    print(untracked_file[0])
+    repo.git.add(untracked_file[0])
     repo.git.commit(m=f"added: {untracked_file[0]}")
 origin = repo.remote(name="origin")
 origin.push()
